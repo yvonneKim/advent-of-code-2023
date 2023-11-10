95abd1ecdfc246fceb5f660d8d119995db148d19
Yvonne Kim <me@yvonne.kim>
2023-11-09 21:22:30 -0500

    Add go.mod, main, mostrings pkg

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yvonneKim/advent-of-code-2023/morestrings"
+)
+
+func main() {
+	fmt.Println("hiello world")
+	fmt.Println(morestrings.ReverseRunes("dlrow olleh"))
+}
